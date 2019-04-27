e56887faf0ccdbe5180aa3dcd364443a99be7d97
--global <mingl19@uci.edu>
2019-04-27 01:01:26 -0700

    detail layout

---
+++ src/pages/detail/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+export const DetailWrapper = styled.div`
+	overflow: hidden;
+    margin: 0 auto;
+    padding-top: 20px;
+    padding-bottom: 40px;
+    width: 620px;
+`
+
+export const Header = styled.div`
+    margin: 20px 0 20px 0;
+	line-height: 44px;
+	color: #333;
+	font-size: 34px;
+	font-weight: 700;
+    line-height: 1.3;
+`
+
+export const Content = styled.div`
+	color: #2f2f2f;
+	img {
+		width:700px;
+	    padding-bottom: 25px;
+	    margin-left: -40px;
+	    text-align: center;
+	    max-width: 700px;
+	    max-height: 467px; 
+	    background-color: transparent;
+	}
+	p {
+		margin: 25px 0;
+		font-size: 16px;
+		line-height: 30px;
+	}
+`
